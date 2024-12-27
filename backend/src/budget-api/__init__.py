538ef9a717d4e58c7e86d3036a97ad2c6b6dce6b
David Sciacchettano <david.sciacchettano@gmail.com>
2024-12-26 19:45:04 -0500

    Remove pycache

--- backend/src/budget-api/__init__.py
+++ backend/src/budget-api/__init__.py
@@ -1,6 +1,24 @@
 from __future__ import annotations
 
 from fastapi import FastAPI
+import logging
 
 def app() -> FastAPI:
+    # Logging config
+    logging_config()
+    
+    app = FastAPI()
+    
+    # Setup Routers
+    routers()
+    
+    return app
+
+def routers():
     pass
+
+def logging_config() -> None:    
+    logger = logging.getLogger("uvicorn.error")
+    logger.setLevel(logging.DEBUG)
+    
+    logger.info("Logger configured")
