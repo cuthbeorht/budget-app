b552604e5d8cae4685606d8d59197bd212f33bda
David Sciacchettano <david.sciacchettano@gmail.com>
2024-12-26 20:27:19 -0500

    Create first test

--- backend/test/unit/http/test_upload_statement.py
+++ backend/test/unit/http/test_upload_statement.py
@@ -1,2 +1,9 @@
-def test_given_valid_statement_upload_expect_successful_api_call():
-    pass
+from fastapi.testclient import TestClient
+
+
+def test_given_valid_statement_upload_expect_successful_api_call(
+    test_client: TestClient
+):
+    response = test_client.post("/statements")
+    
+    assert response.status_code == 200
