18fbc6f40a7013e96431154eb76a16954103842c
David Sciacchettano <david.sciacchettano@gmail.com>
2024-12-26 20:28:10 -0500

    Make test nicer

--- backend/test/unit/http/test_upload_statement.py
+++ backend/test/unit/http/test_upload_statement.py
@@ -7,3 +7,4 @@ def test_given_valid_statement_upload_expect_successful_api_call(
     response = test_client.post("/statements")
     
     assert response.status_code == 200
+    assert response.json() is None
