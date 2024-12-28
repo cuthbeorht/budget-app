2833ca3c42cd0dbdd78b2089e0c9348fedf7eac7
David Sciacchettano <david.sciacchettano@gmail.com>
2024-12-28 08:45:52 -0500

    Make upload test work

--- backend/src/budgeting/statement/http.py
+++ backend/src/budgeting/statement/http.py
@@ -1,12 +1,17 @@
 from __future__ import annotations
 import logging
+from typing import Annotated
 
-from fastapi import APIRouter
+from fastapi import APIRouter, File, HTTPException, UploadFile, status
 
 router = APIRouter(
     prefix="/statements"    
 )
 
 @router.post("/")
-async def upload():
+async def upload(file: UploadFile):
     logging.info("Uploading a statement")
+    
+    if file.size == 0:
+        logging.error(f"File size is zero")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Error with file upload")
--- backend/test/conftest.py
+++ backend/test/conftest.py
@@ -1,8 +1,14 @@
+from pathlib import Path
 from fastapi.testclient import TestClient
 
 from budgeting import app
 import pytest
 
 @pytest.fixture()
+def project_root() -> Path:
+    return Path(__file__).parents[1]
+
+@pytest.fixture()
 def test_client() -> TestClient:
     return TestClient(app())
+
