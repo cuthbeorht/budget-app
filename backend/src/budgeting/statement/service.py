ee2d6bb083864cd9c0286a71d42a9fc1cdc1bd9c
David Sciacchettano <david.sciacchettano@gmail.com>
2024-12-29 19:25:05 -0500

    WIP:

--- backend/src/budgeting/statement/service.py
+++ backend/src/budgeting/statement/service.py
@@ -4,7 +4,7 @@ from typing import BinaryIO
 from budgeting.database import Repository
 
 from budgeting.statement.models import StatementParseCommand
-from ofxparse import OfxParser
+
 
 
 class StatementParserService:
