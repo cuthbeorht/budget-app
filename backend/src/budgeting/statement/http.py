388fcb27fb66c264ecae19db3729e97ef91296ad
David Sciacchettano <david.sciacchettano@gmail.com>
2025-01-01 12:25:55 -0500

    Get file upload working again

--- backend/src/budgeting/statement/http.py
+++ backend/src/budgeting/statement/http.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from budgeting.statement.service import StatementParserService
 import logging, services
 from typing import Annotated
 
@@ -11,10 +12,13 @@ router = APIRouter(
 )
 
 @router.post("/")
-async def upload(file: UploadFile):
+async def upload(
+    stmt: UploadFile, 
+    # service: StatementParserService = Depends(services.statement_parser_service)
+):
     logging.info("Uploading a statement")
     
-    if file.size == 0:
+    if stmt.size == 0:
         logging.error(f"File size is zero")
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Error with file upload")
     
--- backend/test/unit/statement/http/test_upload_statement.py
+++ backend/test/unit/statement/http/test_upload_statement.py
@@ -7,23 +7,25 @@ import pytest
 def empty_statement_contents(
     empty_statement_file: Path
 ) -> str:
-     with open(empty_statement_file, "r") as f:
+     with open(empty_statement_file, "rb") as f:
         return f.read()
 
 def test_given_valid_statement_upload_expect_successful_api_call(
     test_client: TestClient,
-    valid_statement_contents: str
+    valid_statement_contents: str,
+    valid_statement_file: Path
 ):       
-    response = test_client.post("/statements/", files={"file": valid_statement_contents})
+    response = test_client.post("/statements/", files={"stmt": valid_statement_file.open("rb")})
     
     assert response.status_code == 200
     assert response.json() is None
     
 def test_given_empty_statement_upload_execpt_400(
     test_client: TestClient,
-    empty_statement_contents: str
+    empty_statement_contents: str,
+    empty_statement_file: Path
 ):
-    response = test_client.post("/statements/", files={"file": empty_statement_contents})
+    response = test_client.post("/statements/", files={"stmt": empty_statement_file.open("rb") })
     
     assert response.status_code == 400
     assert response.json() == {"detail": "Error with file upload"}
