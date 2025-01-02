77508c771eb7beca5850be235b06b6f0c1bd74ed
David Sciacchettano <david.sciacchettano@gmail.com>
2025-01-02 09:48:51 -0500

    create basic upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,28 @@
 import './App.css'
+import {React} from 'react';
 
 function App() {
+
+  const handleUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log('I am uploading')
+  }
+
   return (
-    <div class="sidebar">
-      <a class="active" href="#home">Home</a>
-      <a href="#news">News</a>
-      <a href="#contact">Contact</a>
-      <a href="#about">About</a>
-    </div>
+    <>
+      <div className="sidebar">
+        <a className="active" href="#home">Home</a>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+      </div>
+      <div className="content">
+        <form>
+          <label>Upload file</label>
+          <input type="file" id="statement" />
+          <input type="submit" onClick={handleUpload} />
+        </form>
+      </div>
+    </>
   );
 }
 
