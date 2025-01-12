92dc2056b1105ee0dd8dfe1cf5198e2e01923385
David Sciacchettano <david.sciacchettano@gmail.com>
2025-01-11 20:06:47 -0500

    Use pure react

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Budgeting from './Budgeting.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Home from './pages/Home/Home.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Budgeting />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/statements" element={<Budgeting />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
